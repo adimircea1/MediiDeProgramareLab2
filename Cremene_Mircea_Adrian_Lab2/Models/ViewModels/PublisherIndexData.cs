094f5d09f397287c2a64d44850e855f21806c370
adimircea1 <adimircea04@gmail.com>
2024-10-28 20:02:16 +0200

    munca la lab

---
+++ Cremene_Mircea_Adrian_Lab2/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,7 @@
+namespace Cremene_Mircea_Adrian_Lab2.Models.ViewModels;
+
+public class PublisherIndexData
+{
+    public IEnumerable<Publisher>? Publishers { get; set; } 
+    public IEnumerable<Book>? Books { get; set; }
+}
